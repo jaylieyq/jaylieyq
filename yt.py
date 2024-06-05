e9e6428288eb0f7e09cb0fb4fbd9eab90612f7f4
jaylieyq <jaylieyq@gmail.com>
2024-06-05 02:18:54 +0100

    Create yt.py

---
+++ yt.py
@@ -0,0 +1,15 @@
+package com.it.dao;
+
+import com.it.entity.Orderdetails;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface OrderdetailsDAO {
+	List<Orderdetails> selectAll(HashMap map);
+	void add(Orderdetails orderdetails);
+	void update(Orderdetails orderdetails);
+	void delete(int id);
+	Orderdetails findById(int id);
+    List<Orderdetails> selectHot(HashMap map);
+}
